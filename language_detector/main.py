c39203fc8d059953ab3f9ad9e1cf4291c8ae1441
Joe <joe.zimny@gmail.com>
2016-07-14 02:52:42 +0000

    Dummy First Change

--- language_detector/main.py
+++ language_detector/main.py
@@ -6,4 +6,5 @@ from .languages import LANGUAGES
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
+    # First JLZ Edit
     pass
