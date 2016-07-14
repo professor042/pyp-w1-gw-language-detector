dd4f729efe4238895a45759f2f88474d22321e16
Joe <joe.zimny@gmail.com>
2016-07-14 04:07:40 +0000

    Passed 4 Make File Tests - Failed Tox

--- language_detector/languages.py
+++ language_detector/languages.py
@@ -3,7 +3,39 @@
 LANGUAGES = [
     {
         'name': 'Spanish',
-        'common_words': ['yo', 'tu', 'el', 'nosotros']  # add your words here
+        'common_words': [
+            'el', 'la', 'de', 'que', 'y', 'a', 'en', 'un', 'ser', 'se',
+            'no', 'haber', 'por', 'con', 'su', 'para', 'como', 'estar',
+            'tener', 'le', 'lo', 'lo', 'todo', 'pero', 'hacer',
+            'o', 'poder', 'decir', 'este', 'ir', 'otro', 'ese', 'la',
+            'si', 'me', 'ya', 'ver', 'porque', 'dar', 'cuando',
+            'muy', 'sin', 'vez', 'mucho', 'saber',  'sobre',
+            'mi', 'alguno', 'mismo', 'yo', 'hasta'
+        ]
     },
     #  include your extra languages here
+    {
+        'name': 'German',
+        'common_words': [
+            'das', 'ist', 'du', 'ich', 'nicht', 'die', 'es', 'und',
+            'der', 'was', 'wir', 'zu', 'ein', 'er', 'in', 'sie', 'mir',
+            'mit', 'ja', 'wie', 'den', 'auf', 'mich', 'dass', 'so',
+            'hier', 'eine', 'wenn', 'hat', 'all', 'sind', 'von',
+            'dich', 'war', 'haben', 'an', 'habe', 'da', 'nein',
+            'bin', 'noch', 'dir', 'uns', 'sich', 'nur',
+            'einen', 'kann', 'dem'
+        ]
+    },
+    
+        {
+        'name': 'English',
+        'common_words': [
+            'is', 'at', 'an', 'the', 'he', 'she', 'or', 'and', 'be',
+            'to', 'of', 'and', 'in', 'that', 'have', 'I', 'it', 'for',
+            'not', 'on', 'with', 'as', 'you', 'do', 'at', 'so', 'up',
+            'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me', 
+            'when', 'make', 'can', 'like', 'time', 'just', 'him', 'know',
+            'take', 'people', 'into', 'year', 'your'
+        ]
+    }
 ]
--- language_detector/main.py
+++ language_detector/main.py
@@ -6,5 +6,31 @@ from .languages import LANGUAGES
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
-    # First JLZ Edit
-    pass
+
+    words = text.split()
+    
+    # Pull languages and initiate a language word count dictionary
+    language_count = {}
+    
+    # l is dictionary with name=language, common_words=list of words
+    for l in languages:
+        language_count[l['name']] = 0
+    
+    # loop through words and add to count of appropriate language
+    for word in words:
+        for l in languages:
+            if word.lower() in l['common_words']:
+                language_count[l['name']] += 1
+    
+    
+    # find language with highest count
+    high_count_language = 'no_language_detected'
+    high_count = 0
+
+    for key, value in language_count.items():
+        if value > high_count:
+            high_count_language = key
+            high_count = value
+
+    return high_count_language
+        
